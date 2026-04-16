5c1a47b34076e1a0c1f6052793ee356b8205e4b4
zoomdong <1344492820@qq.com>
2026-04-16 11:20:22 +0800

    chore: init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.scss';
 const HomePage = () => {
   return (
     <div>
-      <h2 className={styles.title}>Yay! Welcome to umi!</h2>
+      <h2 className={styles.title}>Yay! Welcome to umizzzz!</h2>
       <p>
         <img src={yayJpg} width="388" />
       </p>
