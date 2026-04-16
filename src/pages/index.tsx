7af174fefd942c0e10ee8fc55f9109bf08e1ff5f
zoomdong <1344492820@qq.com>
2026-04-16 16:05:04 +0800

    chore: upgrade umi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ import styles from './index.scss';
 const HomePage = () => {
   return (
     <div>
-      <h2 className={styles.title}>Yay! Welcome to umizzzz!</h2>
+      <h2 className={styles.title}>Yay! Welcome to umi!</h2>
       <p>
-        <img src={yayJpg} width="388" />
+        <img src={yayJpg} width="600" />
       </p>
       <p>
         To get started, edit <code>pages/index.tsx</code> and save to reload.
